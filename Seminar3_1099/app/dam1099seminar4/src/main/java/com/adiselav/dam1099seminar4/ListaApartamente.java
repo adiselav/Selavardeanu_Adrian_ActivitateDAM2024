bb4dc40a7b41adf17ce5decd0ec801a4c58df745
adiselav <adisela2003@gmail.com>
2024-10-28 19:50:47 +0200

    seminar 5

---
+++ Seminar3_1099/app/dam1099seminar4/src/main/java/com/adiselav/dam1099seminar4/ListaApartamente.java
@@ -0,0 +1,57 @@
+package com.adiselav.dam1099seminar4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.List;
+
+public class ListaApartamente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_lista_apartamente);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Intent it=getIntent();
+        List<Apartament> apartamente = it.getParcelableArrayListExtra("apartamente");
+
+        ListView lv = findViewById(R.id.apartamenteLV);
+
+        ArrayAdapter<Apartament> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1,apartamente);
+        lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                Toast.makeText(ListaApartamente.this, apartamente.get(i).toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int i, long id) {
+                apartamente.remove(i);
+                adapter.notifyDataSetChanged();
+                return false;
+            }
+        });
+
+    }
+}
--- Seminar3_1099/app/dam1099seminar4/src/main/java/com/adiselav/dam1099seminar4/MainActivity.java
+++ Seminar3_1099/app/dam1099seminar4/src/main/java/com/adiselav/dam1099seminar4/MainActivity.java
@@ -2,25 +2,32 @@ package com.adiselav.dam1099seminar4;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-
-
+    //declarare lista de obiecte
+    private List<Apartament> apartamente=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        //initializare lista de obiecte
+        apartamente = new ArrayList<>();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -37,5 +44,31 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(it,403);
             }
         });
+
+        Button butonLista=findViewById(R.id.buttonLV);
+        butonLista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it=new Intent(getApplicationContext(), ListaApartamente.class);
+                it.putParcelableArrayListExtra("apartamente", (ArrayList<? extends Parcelable>) apartamente);
+                startActivity(it);
+            }
+        });
+
     }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==403){
+            if (resultCode==RESULT_OK){
+                Apartament apartament=data.getParcelableExtra("apartament");
+                apartamente.add(apartament);
+            }
+        }
+    }
+
+    //end button
 }
