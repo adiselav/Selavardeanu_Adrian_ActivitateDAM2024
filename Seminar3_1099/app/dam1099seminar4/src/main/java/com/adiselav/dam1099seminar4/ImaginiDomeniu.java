933cf81cb719771dc8c0bd280a8720f6357b73f5
Adi Sela <adisela2003@gmail.com>
2024-11-19 15:18:20 +0200

    seminar 8

---
+++ Seminar3_1099/app/dam1099seminar4/src/main/java/com/adiselav/dam1099seminar4/ImaginiDomeniu.java
@@ -0,0 +1,50 @@
+package com.adiselav.dam1099seminar4;
+
+import android.graphics.Bitmap;
+
+public class ImaginiDomeniu {
+    private String textAfisat;
+    private Bitmap imagine;
+    String link;
+
+    public ImaginiDomeniu(String textAfisat, Bitmap imagine, String link) {
+        this.textAfisat = textAfisat;
+        this.imagine = imagine;
+        this.link = link;
+    }
+
+
+    public String getTextAfisat() {
+        return textAfisat;
+    }
+
+    public void setTextAfisat(String textAfisat) {
+        this.textAfisat = textAfisat;
+    }
+
+    public Bitmap getImagine() {
+        return imagine;
+    }
+
+    public void setImagine(Bitmap imagine) {
+        this.imagine = imagine;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("ImaginiDomeniu{");
+        sb.append("textAfisat='").append(textAfisat).append('\'');
+        sb.append(", imagine=").append(imagine);
+        sb.append(", link='").append(link).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
