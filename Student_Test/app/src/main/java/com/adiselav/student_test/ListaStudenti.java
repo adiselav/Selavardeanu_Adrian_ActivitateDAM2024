2c4fa1fb87846e585f1c00aa25f5f70b7dbbfc23
Adi Sela <adisela2003@gmail.com>
2024-12-17 10:35:34 +0200

    student_test

---
+++ Student_Test/app/src/main/java/com/adiselav/student_test/ListaStudenti.java
@@ -0,0 +1,78 @@
+package com.adiselav.student_test;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.room.Room;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class ListaStudenti extends AppCompatActivity {
+    List<Student> studenti = null;
+    StudentDAO studentDAO;
+    StudentDatabase studentDatabase;
+    ArrayAdapter<Student> adapter = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_lista_studenti);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ListView lv = findViewById(R.id.lvStudenti);
+        studentDAO = studentDatabase.getInstance(getApplicationContext()).getStudentiDao();
+
+
+
+        Executor executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    studenti = studentDAO.getStudenti();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }finally{
+                    handler.post(() -> {
+                        adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, studenti);
+                        lv.setAdapter(adapter);
+                    });
+                }
+            }
+        });
+
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                SharedPreferences sp = getSharedPreferences("obiecteFavorite", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString(String.valueOf(studenti.get(i).getId()), studenti.get(i).toString());
+                editor.commit();
+                return false;
+            }
+        });
+    }
+}
